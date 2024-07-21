bf291a714f08670db6c030b627e42dd0cbcba1f5
wqdsdsf123 <chlxodud04@naver.com>
2024-07-21 13:38:41 +0900

    Changes

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -26,7 +26,7 @@ public class LoginController {
     @RequestMapping("HELLO")
     @ResponseBody
     public String HELLO(){
-        return "helloo";
+        return "helloos";
     }
     @RequestMapping(value="login", method = RequestMethod.POST)
     public  String gotoWelcompage(@RequestParam String name, @RequestParam String password, ModelMap model){
