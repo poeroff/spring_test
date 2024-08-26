2d7f8893c4777625b71bf7b88f1fdb84c3957e2d
wqdsdsf <chlxodud04@naver.com>
2024-08-27 01:12:17 +0900

    asd
    ad

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,7 +2,7 @@
 # 애플리케이션을 시작하는 스크립트
 
 # 애플리케이션 디렉토리로 이동
-cd /home/ubuntu/myapp
+cd /home/ubuntu/myapp/target
 
 # Java 애플리케이션 시작
 nohup java -jar myfirstwebapp-0.0.1-SNAPSHOT.jar> /home/ubuntu/myapp/application.log 2>&1 &
