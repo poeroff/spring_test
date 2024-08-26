24e01e3a8b6b03f85672f2d11427d7dd3bc09906
wqdsdsf <chlxodud04@naver.com>
2024-08-27 00:49:59 +0900

    Revert ",,"
    This reverts commit 7028545d454d57a3fbbf488302739a02b5732153.

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -5,6 +5,6 @@
 cd /home/ubuntu/myapp
 
 # Java 애플리케이션 시작
-nohup java -jar myfirstwebapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/myapp/application.log 2>&1 &
+nohup java -jar your-application.jar > /home/ubuntu/myapp/application.log 2>&1 &
 
 echo "Application started"
